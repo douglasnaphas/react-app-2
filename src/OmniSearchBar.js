c7fe19a33f0352951155a142deb3c2ea902565fb
Douglas Naphas <douglasnaphas@gmail.com>
2019-09-24 01:27:04 -0400

    Add files from the Issue illustrating the problem
    https://github.com/airbnb/enzyme/issues/2236

---
+++ src/OmniSearchBar.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const OmniSearchBar = ({ onSubmitSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const valueChanged = evt => {
+    setQuery(evt.target.value);
+  };
+
+  const enterPressed = evt => {
+    const code = evt.keyCode || evt.which;
+    console.log("enterPressed: " + query);
+    if (code === 13 && query.length > 2) {
+      onSubmitSearch(query);
+    }
+  };
+
+  console.log("render: " + query);
+  return (
+    <div className="omni-search-bar">
+      <tr>
+        <input
+          type="search"
+          placeholder="across all accounts"
+          className="search-bar"
+          onChange={valueChanged}
+          value={query}
+          onKeyPress={enterPressed}
+        />
+      </tr>
+    </div>
+  );
+};
+
+OmniSearchBar.propTypes = {
+  onSubmitSearch: PropTypes.func.isRequired
+};
+
+export default OmniSearchBar;
---
+++ src/OmniSearchBar.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import OmniSearchBar from "./OmniSearchBar";
+import { shallow } from "enzyme";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("OmniSearch/OmniSearchPage", () => {
+  let onSubmitFunc;
+
+  beforeEach(() => {
+    onSubmitFunc = jest.fn();
+  });
+
+  it("three characters are required to initiate search", () => {
+    const component = shallow(<OmniSearchBar onSubmitSearch={onSubmitFunc} />);
+    const input = component.find(".search-bar");
+
+    expect(input.exists()).toEqual(true);
+    expect(component).toMatchSnapshot();
+
+    input.simulate("keypress", { keyCode: 13 });
+    expect(onSubmitFunc).not.toHaveBeenCalled();
+
+    input.prop("onChange")({ target: { value: "te" } });
+    expect(component).toMatchSnapshot();
+    input.simulate("keypress", { keyCode: 13 });
+    expect(onSubmitFunc).not.toHaveBeenCalled();
+
+    input.prop("onChange")({ target: { value: "test1234" } });
+    expect(component).toMatchSnapshot();
+    input.simulate("keypress", { keyCode: 13 });
+    expect(onSubmitFunc).toHaveBeenCalled();
+  });
+});
